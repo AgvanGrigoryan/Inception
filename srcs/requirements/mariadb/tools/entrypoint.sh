c76a9b81e0284b232616626a850ea745461defee
Agvan Grigoryan <agvangrigoryan2003@gmail.com>
2025-02-13 23:56:31 +0400

    added secrets passwords to mariadb and set env vars to database

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,21 +1,22 @@
 set -e
 
 # set env variables for db
-if ( -f $MYSQL_ROOT_PASSWORD_FILE ]; then
+if [ -f $MYSQL_ROOT_PASSWORD_FILE ]; then
 	export MYSQL_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD_FILE)
 fi
 
-if ( -f $MYSQL_PASSWORD_FILE ]; then
+if [ -f $MYSQL_PASSWORD_FILE ]; then
 	export MYSQL_PASSWORD=$(cat $MYSQL_PASSWORD_FILE)
 fi
 
 # initialize the database
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
-fi
+#if [ ! -d "/var/lib/mysql/mysql" ]; then
+#	mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
+#fi
 
-if ( -f "/docker-entrypoint-initdb.d/init.sql" ]; then
-	mysql < /docker-entrypoint-initdb.d/init.sql
-fi
+#if [ -f "/docker-entrypoint-initdb.d/init.sql" ]; then
+#	mysql < /docker-entrypoint-initdb.d/init.sql
+#fi
 
-exec mysqld --user=mysql
+exec sleep infinity
+# exec mysqld --user=mysql
