50a9815afffe6bc5c7ef68cbd881c2182fb600cc
Agvan Grigoryan <agvangrigoryan2003@gmail.com>
2025-02-14 00:25:06 +0400

    mariadb works stable, minimal conf and just check ini.sql

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -10,9 +10,9 @@ if [ -f $MYSQL_PASSWORD_FILE ]; then
 fi
 
 # initialize the database
-#if [ ! -d "/var/lib/mysql/mysql" ]; then
-#	mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
-#fi
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
+fi
 
 #if [ -f "/docker-entrypoint-initdb.d/init.sql" ]; then
 #	mysql < /docker-entrypoint-initdb.d/init.sql
