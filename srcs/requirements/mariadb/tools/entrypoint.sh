e4f052a5a08c83751dfc62736cb9fbea2f87792b
Agvan Grigoryan <agvangrigoryan2003@gmail.com>
2025-02-14 13:56:22 +0400

    init.sql works with env vars but envsubst or sed doesn't work

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -14,9 +14,14 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
 fi
 
-#if [ -f "/docker-entrypoint-initdb.d/init.sql" ]; then
-#	mysql < /docker-entrypoint-initdb.d/init.sql
-#fi
-
+if [ -f "/docker-entrypoint-initdb.d/init.sql" ]; then
+	envsubst < /docker-entrypoint-initdb.d/init.sql > /tmp/init.sql
+	sed "s|\b$MYSQL_USER\b|'$MYSQL_USER'|g" | \
+	sed "s|\b$MYSQL_PASSWORD\b|'$MYSQL_PASSWORD'|g" | \
+	sed "s|\b$MYSQL_DATABASE\b|'$MYSQL_DATABASE'|g" > /tmp/init.sql
+	#exec env > /var/lib/mysql/con_env
+	exec cat /tmp/init.sql > /var/lib/mysql/init.sql
+	mysql < /tmp/init.sql
+fi
 exec sleep infinity
 # exec mysqld --user=mysql
